d170a973c14f40a1c2549faa39c76efc62543320
MohGhazy <mohghazyaa@gmail.com>
2025-07-22 21:48:09 +0700

    animating

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,20 @@ hamburger.addEventListener('click', function () {
  hamburger.classList.toggle('hamburger-active');
  navMenu.classList.toggle('hidden');
 });
+
+// Animasi
+document.addEventListener("DOMContentLoaded", function () {
+ AOS.init({
+  duration: 1500,
+  once: true,
+ });
+ new Typed("#typed-name", {
+  strings: ["Mohammad Ghazy"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 500,
+  loop: true,
+  showCursor: true,
+  cursorChar: "|"
+ });
+});
